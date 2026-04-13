eec6b80616d6658688dea395c916531611e1c5b7
zub <ars1for1work@gmail.com>
2026-04-13 23:35:39 +0300

    feat: complete value, hasNext, hasPrev and min/max

--- main.cpp
+++ main.cpp
@@ -14,25 +14,54 @@ struct BTreeIt
   size_t s;
   BTree< T, K >* current;
 };
+template < class T, size_t K >
+BTree< T, K >* minimum(BTree< T, K >* root)
+{
+  if (!root) {
+    return root;
+  }
 
+  while (root->childs[0]) {
+    root = root->childs[0];
+  }
+  return root;
+}
+
+template < class T, size_t K >
+BTree< T, K >* maximum(BTree< T, K >* root)
+{
+  if (!root) {
+    return root;
+  }
+
+  while (root->childs[K]) {
+    root = root->childs[K];
+  }
+  return root;
+}
 template < class T, size_t K >
 T value(BTreeIt< T, K > it)
 {
   return it.current->val[it.s];
 }
 
+template < class T, size_t K >
+BTreeIt< T, K > next(BTreeIt< T, K > it);
 
-template <class T, size_t K>
-BTreeIt<T, K> next(BTreeIt<T, K> it);
+template < class T, size_t K >
+BTreeIt< T, K > prev(BTreeIt< T, K > it);
 
-template <class T, size_t K>
-BTreeIt<T, K> prev(BTreeIt<T, K> it);
+template < class T, size_t K >
+bool hasNext(BTreeIt< T, K > it)
+{
+  return next(it).current != nullptr;
+}
 
-template <class T, size_t K>
-bool hasNext(BTreeIt<T, K> it);
-
-template <class T, size_t K>
-bool hasPrev(BTreeIt<T, K> it);
+template < class T, size_t K >
+bool hasPrev(BTreeIt< T, K > it)
+{
+  return prev(it).current != nullptr;
+}
 
 
 int main()
